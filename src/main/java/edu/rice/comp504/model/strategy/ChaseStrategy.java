3ba5ee9afbf1ba82cd34f28841171631bf7b54bb
Deer-Apple <jasonhailey@163.com>
2019-11-05 16:39:10 -0600

    fix bfs bug

--- src/main/java/edu/rice/comp504/model/strategy/ChaseStrategy.java
+++ src/main/java/edu/rice/comp504/model/strategy/ChaseStrategy.java
@@ -17,12 +17,15 @@ public class ChaseStrategy implements IUpdateStrategy {
 
     private ACellObject[][] board;
 
+    private Deque<Point> cachePath;
+
     /**
      * Constructor.
      */
     private ChaseStrategy(PacMan pacMan, ACellObject[][] board) {
         this.pacman = pacMan;
         this.board = board;
+        cachePath = new LinkedList<>();
     }
 
     public static void cleanStrategy() {
@@ -50,7 +53,6 @@ public class ChaseStrategy implements IUpdateStrategy {
         if (context instanceof Ghost) {
             Ghost ghost = (Ghost) context;
             Deque<Point> path = bfs(ghost, pacman);
-            //System.out.println(path);
             if (path != null) {
                 path.pollFirst();
                 if (!path.isEmpty()) {
@@ -87,9 +89,10 @@ public class ChaseStrategy implements IUpdateStrategy {
         int pacmanCol = (int) Math.round(pacMan.getLocationX() / 31);
         int pacmanRow = (int) Math.round(pacMan.getLocationY() / 31);
         Point pacmanLoc = new Point(pacmanCol, pacmanRow);
-        System.out.println(pacmanLoc);
-        System.out.println(ghostCol);
-        System.out.println(ghostRow);
+//        System.out.println(pacmanLoc);
+//        System.out.println(pacMan.getLocationX() + "," + pacMan.getLocationY());
+//        System.out.println(ghostCol + "," + ghostRow);
+//        System.out.println(ghostRow);
 
         Queue<Deque<Point>> queue = new LinkedList<>();
 
@@ -112,21 +115,26 @@ public class ChaseStrategy implements IUpdateStrategy {
             }
             for (int i = 0; i < 4; i++) {
                 Point newLoc = new Point(lastPoint.x + offsetX[i], lastPoint.y + offsetY[i]);
-                if (visited.contains(newLoc)) {
+                if (visited.contains(newLoc) || outsideBoard(newLoc)) {
+                    continue;
+                }
+                if (board[newLoc.x][newLoc.y] instanceof WallUnit
+                        || board[newLoc.x][newLoc.y] instanceof DoorUnit) {
                     continue;
                 }
                 visited.add(newLoc);
-                if (board[newLoc.x][newLoc.y] instanceof WallUnit
-                    || board[newLoc.x][newLoc.y] instanceof DoorUnit) {
-                    continue;
-                }
                 Deque<Point> newPath = new ArrayDeque<>();
                 newPath.addAll(path);
                 newPath.addLast(newLoc);
                 queue.add(newPath);
             }
         }
-
         return null;
+    }
+
+    private boolean outsideBoard(Point loc) {
+        int height = board.length, width = board[0].length;
+        int row = loc.y, col = loc.x;
+        return row < 0 || row >= height || col < 0 || col >= width;
     }
 }
