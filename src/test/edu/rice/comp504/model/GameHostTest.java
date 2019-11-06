b683e47b1ce6f6b0eaf65aa7e1f85321c37e56e9
Xiaoyong Zhang <smallbearayu@icloud.com>
2019-11-06 16:43:10 -0600

    Add Unit Test

---
+++ src/test/edu/rice/comp504/model/GameHostTest.java
@@ -0,0 +1,7 @@
+package edu.rice.comp504.model;
+
+import junit.framework.TestCase;
+
+public class GameHostTest extends TestCase {
+
+}
---
+++ src/test/edu/rice/comp504/model/cmd/InteractCmdTest.java
@@ -0,0 +1,7 @@
+package edu.rice.comp504.model.cmd;
+
+import junit.framework.TestCase;
+
+public class InteractCmdTest extends TestCase {
+
+}
